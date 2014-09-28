fdaa6fec4744c3639c50f511b9f41a6363fcdce0
asif-mistry <mohdasif.mistry@gmail.com>
2014-09-29 00:25:50 +0530

    added stupid method for problem 5.

--- Euler/Problem5.cs
+++ Euler/Problem5.cs
@@ -17,29 +17,49 @@ namespace Euler
     {
         public void answerProblem()
         {
+
+            //getSmallestMultimple2();
             Console.WriteLine("\nResult ==" + getSmallestMultimple(1,20));
         }
         public long getSmallestMultimple (int smallestNumber, int largestNumber)
         {
-            int smallestMultiple = 0;
+            bool flag = false;
             int count = 0;//starting count from 1, as we are looking for smallest multiple
-            while (smallestMultiple == 0)
+            while (flag == false)
             {
                 count++;
                 for (int i = smallestNumber; i <= largestNumber; i++)
                 {                    
                     if (count%i!=0)
                     {
-                        smallestMultiple=0;
+                        flag=false;
                         break;
                     }
                     else
                     {
-                        smallestMultiple=count;
+                        flag=true;
                     }
                 }
             }
-            return smallestMultiple;
+            return count;
+        }
+
+        public void getSmallestMultimple2()
+        { 
+            int i = 1;
+            for (; ; )
+            {
+                if (i % 1 == 0 && i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 6 == 0 &&
+                    i % 7 == 0 && i % 8 == 0 && i % 9 == 0 && i % 10 == 0 && i % 11 == 0 &&
+                    i % 12 == 0 && i % 13 == 0 && i % 14 == 0 && i % 15 == 0 && i % 16 == 0 &&
+                    i % 17 == 0 && i % 18 == 0 && i % 19 == 0 && i % 20 == 0)
+                {
+                    Console.WriteLine("Result:"+ i);
+                    break;
+                }
+
+                i++;
+            }
         }
     }
 }
