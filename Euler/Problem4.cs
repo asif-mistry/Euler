16c0585d0f1cdefd8318daba1b94273a15a2f456
asif-mistry <mohdasif.mistry@gmail.com>
2014-09-28 22:56:47 +0530

    getLargestNumberFromDigits

--- Euler/Problem4.cs
+++ Euler/Problem4.cs
@@ -19,10 +19,36 @@ namespace Euler
         public void answerProblem()
         {
             long n = 80177108;// 600851475143111;//four million 4 000 000 
-            bool isPali= isPalindrome(n);
-
-            Console.WriteLine("\nResult ==" + isPali);
+            //bool isPali= isPalindrome(n);
+            //getLargestPalindromeForNumberOfDigits(2);
+            Console.WriteLine("\nResult ==" + getLargestNumberFromDigits(7));
         }
+
+        public void getLargestPalindromeForNumberOfDigits(int num)
+        {
+            int numberOfDigits = num;
+            int largestNumberFromDigits = 99;
+            long largesMultiple = 0;
+            for (int i = largestNumberFromDigits; i > 1; i--)
+            {
+                if (largesMultiple != 0)
+                    break;
+
+                for (int j = largestNumberFromDigits; j > 1; j--)
+                {
+                    long largesMultipleTemp=i*j;
+                    if (isPalindrome(largesMultipleTemp))
+                    {
+                        if (largesMultipleTemp > largesMultiple)
+                            largesMultiple = largesMultipleTemp;
+                        Console.WriteLine("\nResult Largest Multiple ==" + largesMultipleTemp);
+                        break;
+                    }
+                }
+            }
+           
+        }
+
         public bool isPalindrome(long num)
         {
             List<long> digitsList = getDigitsList(num);
@@ -48,6 +74,21 @@ namespace Euler
                 num = num /10;
             }
             return digitsList;
+        }
+
+        public long getLargestNumberFromDigits(int digits)
+        {
+            long largestNumber = 9;
+
+            for (int i = digits; i > 1; i--)
+            {
+                int d = 1;
+                for (int j = 0; j < i-1; j++)
+                    d = d * 10;
+                
+                largestNumber += 9 * d;
+            }
+            return largestNumber;
         }
     }
 }
