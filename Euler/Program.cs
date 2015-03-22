6694118ed0f3323345953c5b88f44580e7836658
asif-mistry <mohdasif.mistry@gmail.com>
2015-03-22 21:24:59 +0530

    Solved Problem 11

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -14,7 +14,7 @@ namespace Euler
     {
         static void Main(string[] args)
         {
-            Problem10 p = new Problem10();
+            Problem11 p = new Problem11();
 
             DateTime d = DateTime.Now;
             Console.WriteLine("Time started ==" + d);
